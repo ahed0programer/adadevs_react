3377c8dad6c63461fb544d3f04c69d0250580bc8
abd alsayad <134537357+abdsyd@users.noreply.github.com>
2025-02-07 11:59:35 +0300

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import MyNavbar from './component/navbar/navbar'
+
+function App() {
+  return (
+    <>
+      <MyNavbar/>
+    </>
+  )
+}
+
+export default App
